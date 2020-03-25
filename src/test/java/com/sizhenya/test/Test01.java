b1396f891cad3c140571816aee3a2a6c36dabacb
whd <sizhenya@qq.com>
2020-03-25 15:33:36 +0800

    day01

---
+++ src/test/java/com/sizhenya/test/Test01.java
@@ -0,0 +1,14 @@
+package com.sizhenya.test;
+/*
+*
+*@author 司振亚
+*@date 2020年3月25日---下午3:29:23
+*
+*/
+public class Test01 {
+
+	public static void main(String[] args) {
+		System.out.println("helloWord");
+	}
+}
+
